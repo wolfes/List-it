acfafed5cf14a3d9f42d26da9970793a7b923ba4
Wolfe Styke <wstyke@mit.edu>
2013-01-31 20:36:37 -0800

    Fixed style!

--- background/model.js
+++ background/model.js
@@ -1,5 +1,5 @@
 /**
- * @filedesc Model API: Stores notes, activity logs, and user info.
+ * Model API: Stores notes, activity logs, and user info.
  *
  * @author: wstyke@gmail.com - Wolfe Styke
  */
@@ -10,40 +10,42 @@ var model = model || {};
  * Sets up all model components.
  */
 model.setupModel = function() {
-  //db.notes.setupTable();
-  db.nstore.setup();
-  //db.logs.setupTable();
+    //db.notes.setupTable();
+    db.nstore.setup();
+    //db.logs.setupTable();
 };
 
 var shuffle = function(v) {
-  return v;
+    return v;
 };
 
 /**
  * Asks model to publish undeleted notes.
  */
 model.publishUndeletedNotes = function() {
-  db.nstore.getUndeletedNotes(function(notes) {
-    // Undeleted notes passed back from db.
-    debug('Publishing: model.UNDELETED_NOTES');
-    model.publisher.trigger(model.EventType.UNDELETED_NOTES, notes);
-  });
+    db.nstore.getUndeletedNotes(function(notes) {
+        // Undeleted notes passed back from db.
+        debug('Publishing: model.UNDELETED_NOTES');
+        model.publisher.trigger(model.EventType.UNDELETED_NOTES, notes);
+    });
 };
 
 /**
  * Publish newOpenHotkey in data to sidebars.
+ * @param {Object} data The hotkey data to publish.
  */
 model.publishNewOpenHotkey = function(data) {
-  model.publisher.trigger(model.EventType.NEW_OPEN_HOTKEY, data);
+    model.publisher.trigger(model.EventType.NEW_OPEN_HOTKEY, data);
 };
 
 /**
- * Adds a note to the model & publishes NOTE_ADD event. 
- * @param {Object} note Note object to add.
+ * Adds a note to the model & publishes NOTE_ADD event.
+ * @param {Object} event Event with note object to add.
+ * !FIXME! Is it supposed to trigger NOTE_ADD with event?
  */
 model.addNote = function(event) {
-  model.publisher.trigger(model.EventType.NOTE_ADD, event);
-  db.nstore.upsertNote(event.note);
+    model.publisher.trigger(model.EventType.NOTE_ADD, event);
+    db.nstore.upsertNote(event.note);
 };
 
 /**
@@ -51,12 +53,16 @@ model.addNote = function(event) {
  * @param {Object} note Note object to save.
  */
 model.saveNote = function(note) {
-  db.nstore.upsertNote(note);
-  model.publisher.trigger(model.EventType.NOTE_SAVE, note);
+    db.nstore.upsertNote(note);
+    model.publisher.trigger(model.EventType.NOTE_SAVE, note);
 };
 
+/**
+ * Returns the magical note with note order info from nstore.
+ * @return {Object} The magical note with note order.
+ */
 model.getNoteOrder = function() {
-  return db.nstore.getMagicNote();
+    return db.nstore.getMagicNote();
 };
 
 /**
@@ -64,9 +70,9 @@ model.getNoteOrder = function() {
  * @param {Object} magicNote Magic Note object to save.
  */
 model.saveNoteOrder = function(magicNote) {
-  debug('saving magicNote:', magicNote.jid);
-  db.nstore.upsertNote(magicNote);
-  model.publisher.trigger(model.EventType.NOTE_ORDER_CHANGE, magicNote);
+    debug('saving magicNote:', magicNote.jid);
+    db.nstore.upsertNote(magicNote);
+    model.publisher.trigger(model.EventType.NOTE_ORDER_CHANGE, magicNote);
 };
 
 /**
@@ -74,8 +80,8 @@ model.saveNoteOrder = function(magicNote) {
  * @param {Object} note Note object to save.
  */
 model.deleteNote = function(note) {
-  db.nstore.upsertNote(note);
-  model.publisher.trigger(model.EventType.NOTE_DELETE, note);
+    db.nstore.upsertNote(note);
+    model.publisher.trigger(model.EventType.NOTE_DELETE, note);
 };
 
 /**
@@ -83,9 +89,9 @@ model.deleteNote = function(note) {
  * @param {function} processNotes Called with list of all notes.
  */
 model.getAllNotes = function(processNotes) {
-  db.nstore.getAllNotes(function(notes) {
-    processNotes(notes);
-  });
+    db.nstore.getAllNotes(function(notes) {
+        processNotes(notes);
+    });
 };
 
 /**
@@ -93,10 +99,10 @@ model.getAllNotes = function(processNotes) {
  * @param {object} noteOrder The note with the new note-order.
  */
 model.updateNoteOrder = function(noteOrder) {
-  db.nstore.upsertNote(noteOrder);
-  model.publisher.trigger(model.EventType.UPDATE_NOTE_ORDER,
-			  noteOrder)
-}
+    db.nstore.upsertNote(noteOrder);
+    model.publisher.trigger(
+        model.EventType.UPDATE_NOTE_ORDER, noteOrder);
+};
 
 /**
  * Inserts all notes in list.  Optionally publish additions.
@@ -104,12 +110,12 @@ model.updateNoteOrder = function(noteOrder) {
  * @param {boolean} opt_publishEvent True <=> publish changes.
  */
 model.insertBatchNotes = function(notes, opt_publishEvent) {
-  //notes.reverse();
-  db.nstore.insertBatchNotes(notes);
-  if (opt_publishEvent) {
-    model.publisher.trigger(model.EventType.BATCH_INSERT_NOTES,
-			    notes);
-  }
+    //notes.reverse();
+    db.nstore.insertBatchNotes(notes);
+    if (opt_publishEvent) {
+        model.publisher.trigger(model.EventType.BATCH_INSERT_NOTES,
+                notes);
+    }
 };
 
 /**
@@ -118,24 +124,24 @@ model.insertBatchNotes = function(notes, opt_publishEvent) {
  * @param {boolean} opt_publishEvent True <=> publish changes.
  */
 model.upsertBatchNotes = function(notes, opt_publishEvent) {
-  db.nstore.upsertBatchNotes(notes);
-  if (opt_publishEvent) {
-    model.publisher.trigger(model.EventType.BATCH_UPDATED_NOTES,
-			    notes);
-  }
+    db.nstore.upsertBatchNotes(notes);
+    if (opt_publishEvent) {
+        model.publisher.trigger(model.EventType.BATCH_UPDATED_NOTES,
+                notes);
+    }
 };
 
 /**
  * Adds activity log to model.
- * @param {Object} log The Activity Log object to save.
+ * @param {Object} activityLog The Activity Log object to save.
  */
 model.addActivityLog = function(activityLog) {
-  //TODO(wstyke): Implement log saving code: db.lstore.addLog(log);
-  try {
-    db.lstore.insertLog(activityLog);
-  } catch (err) {
-    debug("Failed to save activity log:", err);
-  }
+    //TODO(wstyke): Implement log saving code: db.lstore.addLog(log);
+    try {
+        db.lstore.insertLog(activityLog);
+    } catch (err) {
+        debug('Failed to save activity log:', err);
+    }
 };
 
 /**
@@ -143,8 +149,8 @@ model.addActivityLog = function(activityLog) {
  * @return {Object} A list of activity logs.
  */
 model.getAllLogs = function() {
-  var logs = db.lstore.getAllLogs();
-  return logs;
+    var logs = db.lstore.getAllLogs();
+    return logs;
 };
 
 /**
@@ -152,7 +158,7 @@ model.getAllLogs = function() {
  * @param {number} when Integer timestamp.
  */
 model.deleteLogsBefore = function(when) {
-  db.lstore.deleteBefore(when);
+    db.lstore.deleteBefore(when);
 };
 
 // User Login Information
@@ -164,16 +170,16 @@ model.deleteLogsBefore = function(when) {
  * @param {boolean} opt_couhes True if user is participating in study.
  */
 model.setValidUserInfo = function(email, password, opt_couhes) {
-  db.info.setUserLoginData(email, password);
-  if (opt_couhes) {
-    db.info.setUserCouhes(opt_couhes);
-  }
-  model.publisher.trigger(
-    model.EventType.USER_VALIDATED, {
-      email: email,
-      password: password,
-      couhes: opt_couhes
-    });
+    db.info.setUserLoginData(email, password);
+    if (opt_couhes) {
+        db.info.setUserCouhes(opt_couhes);
+    }
+    model.publisher.trigger(
+            model.EventType.USER_VALIDATED, {
+                email: email,
+        password: password,
+        couhes: opt_couhes
+            });
 };
 
 /**
@@ -182,33 +188,33 @@ model.setValidUserInfo = function(email, password, opt_couhes) {
  * @param {string} password The user's password.
  */
 model.publishInvalidUserInfo = function(email, password) {
-  model.publisher.trigger(
-    model.EventType.USER_INVALID, {
-      email: email,
-      password: password
-    });
+    model.publisher.trigger(
+            model.EventType.USER_INVALID, {
+                email: email,
+        password: password
+            });
 };
 
 /**
  * Publishes that server was not reachable.
  */
 model.publishServerDisconnect = function() {
-  model.publisher.trigger(
-    model.EventType.SERVER_UNREACHABLE, {});
+    model.publisher.trigger(
+            model.EventType.SERVER_UNREACHABLE, {});
 };
 
 /**
  * Publishes whether a user is logged in or not.
  */
 model.publishLoginState = function() {
-  var loggedIn = db.info.isUserLoggedIn();
-  var email = db.info.getUserEmail();
-  model.publisher.trigger(
-    model.EventType.USER_LOGIN_STATUS, {
-      loggedIn: loggedIn,
-      email: email
-    }
-  );
+    var loggedIn = db.info.isUserLoggedIn();
+    var email = db.info.getUserEmail();
+    model.publisher.trigger(
+            model.EventType.USER_LOGIN_STATUS, {
+                loggedIn: loggedIn,
+        email: email
+            }
+            );
 };
 
 
@@ -218,8 +224,8 @@ model.publishLoginState = function() {
  * @return {string} email User's email for server syncing.
  */
 model.getUserEmail = function() {
-  var email = db.info.getUserEmail();
-  return email;
+    var email = db.info.getUserEmail();
+    return email;
 };
 
 
@@ -228,8 +234,8 @@ model.getUserEmail = function() {
  * @return {string} hashpass User's hashpass for server syncing.
  */
 model.getUserHashPass = function() {
-  var hashpass = db.info.getUserHashPass();
-  return hashpass;
+    var hashpass = db.info.getUserHashPass();
+    return hashpass;
 };
 
 /**
@@ -237,17 +243,16 @@ model.getUserHashPass = function() {
  * @param {boolean} opt_syncSuccess True if last sync was successful.
  */
 model.publishSyncSuccess = function(opt_syncSuccess) {
-  var syncSuccess = (opt_syncSuccess !== undefined ? 
-		     opt_syncSuccess :
-		     db.info.getSyncSuccess());
-  db.info.setSyncSuccess(syncSuccess);
-  var email =  db.info.getUserEmail();
-  var msgType = (syncSuccess ?
-		 model.EventType.SYNC_SUCCESS :
-		 model.EventType.SYNC_FAILURE);
-  model.publisher.trigger(msgType, {
-    email: email
-  });
+    var syncSuccess = (opt_syncSuccess !== undefined ?
+            opt_syncSuccess : db.info.getSyncSuccess());
+    db.info.setSyncSuccess(syncSuccess);
+    var email = db.info.getUserEmail();
+    var msgType = (syncSuccess ?
+            model.EventType.SYNC_SUCCESS :
+            model.EventType.SYNC_FAILURE);
+    model.publisher.trigger(msgType, {
+        email: email
+    });
 };
 
 /**
@@ -256,8 +261,8 @@ model.publishSyncSuccess = function(opt_syncSuccess) {
  * @return {boolean} loginState True if last sync was successful.
  */
 model.getLoginState = function() {
-  var loginState = db.info.getSyncSuccess();
-  return loginState;
+    var loginState = db.info.getSyncSuccess();
+    return loginState;
 };
 
 
@@ -265,14 +270,14 @@ model.getLoginState = function() {
  * Clears all user information & publishes LOGOUT event.
  */
 model.logoutUser = function() {
-  db.info.clearUserInfo();
-  model.deleteAllNotes();
-  model.publisher.trigger(model.EventType.LOGOUT, {});
+    db.info.clearUserInfo();
+    model.deleteAllNotes();
+    model.publisher.trigger(model.EventType.LOGOUT, {});
 };
 
 /**
  * Drops and reinstantiates notes table, deleting all notes.
  */
 model.deleteAllNotes = function() {
-  db.nstore.deleteAllNotes();
+    db.nstore.deleteAllNotes();
 };
